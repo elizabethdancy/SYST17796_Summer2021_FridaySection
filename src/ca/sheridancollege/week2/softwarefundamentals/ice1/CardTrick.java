15a3ebd8bacd3dca200aed69ca952c2446e7de50
Kiu So <Kiu So@192.168.0.20>
2021-05-28 15:19:17 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -33,18 +33,18 @@ public class CardTrick {
         
         //insert code to ask the user for Card value and suit, create their card
         Scanner input = new Scanner(System.in);
-        Card g = new Card();
+        Card luckyCard = new Card();
         System.out.println("Pick a card value: 1-13");
         int gValue = input.nextInt();
-        g.setValue(gValue);
+        luckyCard.setValue(gValue);
         System.out.println("Pick a suit!: 1(Hearts), 2(Diamonds), 3(Spades), 4(Clubs)");
         int gSuit = input.nextInt();
-        g.setSuit(Card.SUITS[gSuit-1]);
+        luckyCard.setSuit(Card.SUITS[gSuit-1]);
         
         // and search magicHand here
         boolean match = true;
         for(int i=0; i<magicHand.length; i++){
-            if(g.getValue() == magicHand[i].getValue() && g.getSuit() == magicHand[i].getSuit()){
+            if(luckyCard.getValue() == magicHand[i].getValue() && luckyCard.getSuit() == magicHand[i].getSuit()){
                 match = true;
                 break;
             } else {
