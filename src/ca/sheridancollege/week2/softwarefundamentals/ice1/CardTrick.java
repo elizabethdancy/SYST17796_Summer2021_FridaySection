dec022e6cfa01f712d5dc8167b1d52bc89014f98
Lucas <Lucas@10.0.0.52>
2021-05-28 13:25:06 -0400

    commit java test 1 add notes

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.week2.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye 
+ * @modifier Lucas Donegan doneganl 991637725 2021-05-28
  */
 public class CardTrick {
     
