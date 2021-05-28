4a537b51bd7131b2d737c40f052ab8bec69ef71f
BingChen-Sun <77631070+Happy-Meals@users.noreply.github.com>
2021-05-28 13:28:04 -0400

    Update Card.java

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
@@ -12,7 +12,9 @@ package ca.sheridancollege.week2.softwarefundamentals.ice1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
+ * @author BingChen Sun
+ * Student Number 991622161
+ * @modifier dancye
  */
 public class Card {
 
