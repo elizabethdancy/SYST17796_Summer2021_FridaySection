424897d19d2a77fd402df034f6d5e73ed59b7e44
Lucas <Lucas@10.0.0.52>
2021-05-28 13:15:13 -0400

    commit java test 1

---
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week2.softwarefundamentals.ice1;
+
+/**
+ *
+ * @author Lucas
+ */
+public class NewClass {
+    
+}
