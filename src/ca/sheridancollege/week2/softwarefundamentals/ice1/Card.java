e0977e01a5a5ac21786c054c55df7fcc9a4b22a9
nusrath <nusrath@192.168.2.130>
2021-05-29 13:38:23 -0400

    Nusrath new Branch

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
@@ -7,21 +7,23 @@ package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author dancye
+ * @author Nusrath 99121519
  */
 public class Card {
-
+   
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
+  
+   
+     /** @return the suit
      */
     public String getSuit() {
         return suit;
--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -1,33 +1,63 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @author Nusrath 991627519
  */
 public class CardTrick {
     
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
+        
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+        Random random = new Random();
+        Scanner input= new Scanner(System.in);
+
+        for (int i=0; i<magicHand.length; i++) {
+            
             Card c = new Card();
+            
             //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // add c to your magicHand
+            c.setValue(random.nextInt(14)+1);
+            //c.setSuit(Card.SUITS[insert call to random numberbetween 0-3 here])
+            c.setSuit(Card.SUITS[(random.nextInt(4))]);
+            //add c to your magicHand
+            magicHand[i] = c;
+         System.out.println(c.getValue() + " of " + c.getSuit());
+
         }
-        
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+       
+       System.out.println("\nPick any card value from 1-13: ");
+        int getValue= input.nextInt();
+        
+        System.out.println("Pick any card Suit");
+        String getSuits = input.next(); 
+        
+        for (Card magicHand1 : magicHand) {
+            
+        if(getValue== magicHand1.getValue() && getSuits.equals(magicHand1.getSuit()))
+        System.out.println("It matches!");
+        else {
+            System.out.println("Does not Match");
+        }
+        }
     }
-    
+        
 }
+
+
+    
+
