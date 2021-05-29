ad262f6381297b72b439ce051d250645c8c6038a
elizabethdancy <39062081+elizabethdancy@users.noreply.github.com>
2021-05-29 14:31:02 -0400

    Update Card.java
    as requested

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
@@ -1,4 +1,8 @@
-*/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
 /**
