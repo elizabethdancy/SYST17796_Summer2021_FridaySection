f3927e532f3b184e4d792766003dd97c6903bdf3
xianjun-wang <77976652+xianjun-wang@users.noreply.github.com>
2021-05-30 13:51:18 -0400

    Update Card.java
    Add a Card constructor

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
@@ -25,6 +25,12 @@ public class Card {
     /**
      * @return the suit
      */
+   
+   public Card(int value, String suit) {
+      this.value = value;
+      this.suit = suit;
+   }
+   
     public String getSuit() {
         return suit;
     }
