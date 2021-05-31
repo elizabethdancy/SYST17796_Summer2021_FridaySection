18c71ef6d593423dafb5fcc67c7dc3cdd43d0de4
Karman Singh Dhadda <Karman Singh Dhadda@DESKTOP-EVP7FT4>
2021-05-31 18:40:16 +0530

    last commi

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
@@ -10,7 +10,7 @@ package ca.sheridancollege.week2.softwarefundamentals.ice1;
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
+ * This code is to be used in ICE1. When you create your own branch,,
  * add your name as a modifier.
  * @author dancye, changing a comment
  */
