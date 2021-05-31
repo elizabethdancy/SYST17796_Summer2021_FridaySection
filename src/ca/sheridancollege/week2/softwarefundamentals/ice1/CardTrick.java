96bddc5534d3175d9284e7efd461fc4dc54968f0
gabe905 <gabe905@localhost>
2021-05-31 01:25:57 -0400

    renamed package

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -37,11 +37,11 @@ public class CardTrick {
         System.out.println("Please enter the Card value between 1 & 13: ");//insert code to ask the user for Card value and suit, create their card
         int cardValue;
         cardValue = input.nextInt();
-        luckyCard.setValue(14);
+        luckyCard.setValue(9);
         
         System.out.println("Please enter the Card suit out of these options: 1(Clubs), 2(Diamonds), 3(Spades), 4(Hearts)");
         String cardSuit = input.next();
-        luckyCard.setSuit(Card.SUITS[3]);
+        luckyCard.setSuit(Card.SUITS[2]);
         
         
         // and search magicHand here
@@ -49,14 +49,15 @@ public class CardTrick {
         for (Card magicHand1 : magicHand) {
             if (luckyCard.getValue() == magicHand1.getValue() && luckyCard.getSuit().equals(magicHand1.getSuit())) {
                 magicCard = true;
-            }           
+            } else {
+                magicCard = false;
+            }          
             //Then report the result here
             if (magicCard) {
                 System.out.println("You have won, the card is in the magic hand");
             } else {
                 System.out.println("You have lost, the card is not in the magic hand");
             }
-        }
-    
-}
+        }         
+    }
 }
