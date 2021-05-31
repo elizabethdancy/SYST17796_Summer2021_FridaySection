f37db9933001164e1370028c192a2e67a761a4b1
William Ibarra <ibarraw@sheridancollege.ca>
2021-05-31 16:17:24 -0400

    New branch PartTimeStudent

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
@@ -4,50 +4,57 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
-
 /**
- * A class that models playing card Objects. Cards have 
+ * A class that models playing card Objects. Cards have
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye, changing a comment
+ *
+ * @author dancye
+ * @author William Ibarra Student ID: 991625901 modified on May 28, 2021
  */
-public class Card {
+public class Card
+{
 
-   private String suit; //clubs, spades, diamonds, hearts
+   private String suit; //hearts, diamonds, spades, clubs
    private int value;//1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
-    public String getSuit() {
-        return suit;
-    }
+   public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
 
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
+   /**
+    * @return the suit
+    */
+   public String getSuit ()
+   {
+      return suit;
+   }
 
-    /**
-     * @return the value
-     */
-    public int getValue() {
-        return value;
-    }
+   /**
+    * @param suit the suit to set
+    */
+   public void setSuit (String suit)
+   {
+      this.suit = suit;
+   }
 
-    /**
-     * @param value the value to set
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
-   
-   
-    
+   /**
+    * @return the value
+    */
+   public int getValue ()
+   {
+      return value;
+   }
+
+   /**
+    * @param value the value to set
+    */
+   public void setValue (int value)
+   {
+      this.value = value;
+   }
+
+
+
 }
