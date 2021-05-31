5e2f8298c0502aabc809993ffd9e5f5c8b4f2bcf
arushi1223 <84975044+arushi1223@users.noreply.github.com>
2021-05-31 22:16:28 +0530

    Update CardTrick.java
    Added a description

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -23,6 +23,7 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here),
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // add c to your magicHand.
+            //().
         }
         
         //insert code to ask the user for Card value and suit, create their card
@@ -51,6 +52,7 @@ public class CardTrick {
         }
     }
                //finally complete.
+        
  
     }
         }
