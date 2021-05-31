7143bae9209c1a36f3a70aa015255159754b2d78
Exiusz <Exiusz@192.168.0.158>
2021-05-31 02:32:02 -0400

    new branch added self as author in header

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
@@ -7,12 +7,13 @@ package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
 /**
  * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
+ * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author dancye, changing a comment
+ * @author Mario Martinez (991 362 634)
  */
 public class Card {
 
--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.week2.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @author Mario Martinez (991 362 634)
  */
 public class CardTrick {
     
