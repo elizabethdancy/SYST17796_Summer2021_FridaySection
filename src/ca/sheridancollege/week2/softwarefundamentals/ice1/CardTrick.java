6531cc6d556a59b96a19c1a5d5fd4181250415c2
91836 <91836@DESKTOP-U0C8FQ5>
2021-05-31 22:11:25 +0530

    Commit

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -6,9 +6,9 @@
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
 /**
- * A class that fills a magic hand of 7 cards with random Card Objects
+ * A class that fills a magic hand of 7 cards with random Card Objects,
  * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
+ * for the match to the user's card.. To be used as starting code in ICE 1
  * @author dancye
  */
 public class CardTrick {
@@ -20,14 +20,38 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
+            //c.setValue(insert call to random number generator here),
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // add c to your magicHand
+            // add c to your magicHand.
         }
         
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
+        // and search magic and here
         //Then report the result here
+        Scanner input = new Scanner(System.in);
+        Card luckycard = new Card();
+        System.out.println("Please enter a card between 1 and 13");
+        int gvalue = input.nextInt();
+        luckycard.setValue(gvalue);
+    System.out.println("Enter a suit: 1(Heart), 2(Diamonds), 3(Spades) , 4(clubs)");
+    int gsuites = input.nextInt();
+    luckycard.setsuit{card.suits[gsuits-1]);
+    // and see magicHand here 
+    boolean match = true;
+    for ( int i=0; i<magicHand.length;i++)(
+        
+        if(luckycard.getvalue() == magicHand[i].getvalue()  && luckycard.getsuit == magicHand[i].getsuit()))(
+        available = true;
+        else (available=false;)
+        if(available){
+            System.out.println("Congrats, you won");
+          else()
+          System.out.println("You lost")  ;
+        }
+        }
     }
-    
+               //finally complete.
+ 
+    }
+        }
 }
