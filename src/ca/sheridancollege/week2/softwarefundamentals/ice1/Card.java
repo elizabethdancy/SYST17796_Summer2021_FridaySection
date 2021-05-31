daa948c52c91ae33497b2fe86e96335a647218a2
Exiusz <Exiusz@192.168.0.158>
2021-05-31 03:28:18 -0400

    created code to randomly select cards for magic hand

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/Card.java
@@ -6,21 +6,44 @@
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
+ * A class that models playing card Objects. Cards have a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13) A
+ * suit (clubs, hearts, spades, diamonds). There are 52 cards in a deck, no jokers. This code is to be used in ICE1.
+ * When you create your own branch, add your name as a modifier.
+ *
  * @author dancye, changing a comment
  * @author Mario Martinez (991 362 634)
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    private String suit; //clubs, spades, diamonds, hearts
+    private int value;//1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+    public Card(int randomSuit, int randomValue) {
+        if (randomSuit == 1) {
+            // "Hearts"
+            setSuit(SUITS[0]);
+        } else if (randomSuit == 2) {
+            // "Diamonds"
+            setSuit(SUITS[1]);
+        } else if (randomSuit == 3) {
+            // "Spades"
+            setSuit(SUITS[2]);
+        } else if (randomSuit == 4) {
+            // "Clubs"
+            setSuit(SUITS[3]);
+        } else {
+            throw new IllegalArgumentException("randomSuit " + randomSuit + "is out of range! (1-4)");
+        }
+        
+        if (randomValue >= 1 && randomValue <= 13) {
+            setValue(randomValue);
+        } else {
+            throw new IllegalArgumentException("randomValue" + randomValue + "is out of range! (1-13)");
+        }
+    }
+
     /**
      * @return the suit
      */
@@ -48,7 +71,10 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
-   
-   
     
+    @Override
+    public String toString() {
+        return String.format("Suit: %s\n Value: %s\n", suit, value);
+    }
+
 }
--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -17,13 +19,17 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        int randomSuit, randomValue;
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // add c to your magicHand
+            randomSuit = ThreadLocalRandom.current().nextInt(1, 4 + 1);
+            randomValue = ThreadLocalRandom.current().nextInt(1, 13 + 1);
+            
+            Card c = new Card(randomSuit, randomValue);
+            magicHand[i] = c;
+            
+            // Code to test that all card combinations are coming up properly
+            //System.out.println(magicHand[i].toString());
         }
         
         //insert code to ask the user for Card value and suit, create their card
